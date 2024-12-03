a53a6ed394191f87fe3c6b7805c1e3e6a9187950
Angelo Ndifor Acheregwa Ndonui <angelondifor.acheregwandonui@etu.cegepdrummond.ca>
2024-12-03 16:24:53 -0500

    Ajoutes des icones

--- app/src/main/java/com/example/project_2377432/MainActivity.kt
+++ app/src/main/java/com/example/project_2377432/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -13,8 +15,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.MusicNote
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.rounded.Album
+import androidx.compose.material.icons.rounded.LibraryMusic
+import androidx.compose.material.icons.rounded.Mic
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -24,16 +31,24 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.project_2377432.ui.theme.Project_2377432Theme
 
 
@@ -43,15 +58,18 @@ sealed class Screen(
     val icon: ImageVector
 ) {
     object Home : Screen(
-        route = "home",
-        title = "Accueil",
-        icon = Icons.Default.Home
+        route = "music",
+        title = "Music",
+        icon = Icons.Default.MusicNote
+
     )
     object Profile : Screen(
-        route = "profile",
+        route = "profile/{userId}",
         title = "Profil",
         icon = Icons.Default.Person
-    )
+    ) {
+        fun createRoute(userId: Int) = "profile/$userId"
+    }
     object Settings : Screen(
         route = "settings",
         title = "Paramètres",
@@ -81,7 +99,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(navController: NavController) {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -90,20 +108,41 @@ fun HomeScreen() {
         verticalArrangement = Arrangement.Center
     ) {
         Icon(
-            Icons.Default.Home,
+            Icons.Rounded.LibraryMusic,
             contentDescription = null,
             modifier = Modifier.size(48.dp)
         )
+
         Spacer(modifier = Modifier.height(16.dp))
+
         Text(
             "Écran d'accueil",
             style = MaterialTheme.typography.headlineMedium
         )
+
+        (1..4).forEach {
+            Button(onClick = {
+                navController.navigate(Screen.Profile.createRoute(userId = it))
+                {
+                    // Évite l'empilement des destinations
+                    popUpTo(navController.graph.startDestinationId) {
+                        saveState = true
+                    }
+                    // Évite les copies multiples de la même destination
+                    launchSingleTop = true
+                    // Restaure l'état lors de la reselection
+                    restoreState = false
+                }
+
+            }) {
+                Text(it.toString())
+            }
+        }
     }
 }
 
 @Composable
-fun ProfileScreen() {
+fun ProfileScreen(userId: Int) {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -121,6 +160,11 @@ fun ProfileScreen() {
             "Écran de profil",
             style = MaterialTheme.typography.headlineMedium
         )
+        Text(
+            userId.toString(),
+            style = MaterialTheme.typography.headlineMedium
+        )
+
     }
 }
 
@@ -145,7 +189,7 @@ fun SettingsScreen() {
         )
     }
 }
-
+@Preview
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppNavigation() {
@@ -156,12 +200,38 @@ fun AppNavigation() {
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text("Mon Application") }
+                title = {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(25.dp)
+                    ) {
+                        Icon(
+                            Icons.Rounded.Album,
+                            contentDescription = null,
+                            modifier = Modifier.size(48.dp)
+                        )
+                        Text(
+                            text = ("Kendrick Lamar"),
+                            style = MaterialTheme.typography.titleLarge,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Icon(
+                            Icons.Rounded.Album,
+                            contentDescription = null,
+                            modifier = Modifier.size(48.dp)
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                    actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                )
             )
         },
         bottomBar = {
             NavigationBar {
-                Screen.items.forEach { screen ->
+                Screen.items.filter { it.title != "Profil" }.forEach { screen ->
                     NavigationBarItem(
                         icon = {
                             Icon(screen.icon, contentDescription = screen.title)
@@ -191,10 +261,14 @@ fun AppNavigation() {
             modifier = Modifier.padding(paddingValues)
         ) {
             composable(Screen.Home.route) {
-                HomeScreen()
+                HomeScreen(navController)
             }
-            composable(Screen.Profile.route) {
-                ProfileScreen()
+            composable(
+                route = Screen.Profile.route,
+                arguments = listOf(navArgument("userId") { type = NavType.IntType })
+            ) { backStackEntry ->
+                val userId = backStackEntry.arguments?.getInt("userId") ?: 0
+                ProfileScreen(userId)
             }
             composable(Screen.Settings.route) {
                 SettingsScreen()
