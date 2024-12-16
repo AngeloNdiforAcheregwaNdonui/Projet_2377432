23352b8ca16ca25de5b86ba1014d3369de762dab
Angelo Ndifor Acheregwa Ndonui <angelondifor.acheregwandonui@etu.cegepdrummond.ca>
2024-12-15 22:39:06 -0500

    Ajoutes d'un Search Bar

--- app/src/main/java/com/example/project_2377432/MainActivity.kt
+++ app/src/main/java/com/example/project_2377432/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.project_2377432
 
+import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -19,14 +21,12 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.MusicNote
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.rounded.Album
-import androidx.compose.material.icons.rounded.LibraryMusic
-import androidx.compose.material.icons.rounded.Mic
-import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -35,20 +35,24 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavType
@@ -239,6 +243,23 @@ fun AlbumItem(imageUrl: String, title: String) {
         }**/
 
 
+@Composable
+private fun SearchTextFields(
+            nameSearch: String,
+            onNameChange: (String) -> Unit
+        ) {
+            Row(modifier = Modifier.padding(top = 10.dp)) {
+                TextField(
+                    value = nameSearch,
+                    label = { Text(text = stringResource(R.string.name_label)) },
+                    onValueChange = onNameChange,
+                    modifier = Modifier
+                        .fillMaxWidth(0.67f)
+                        .padding(start = 20.dp)
+                )
+            }
+        }
+
 
 @Composable
 fun ProfileScreen(userId: Int) {
@@ -288,7 +309,7 @@ fun SettingsScreen() {
         )
     }
 }
-@Preview
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppNavigation() {
